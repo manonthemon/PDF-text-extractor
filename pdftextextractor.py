4ae95f566d91aa82041ec183b5fd3e9f8e4f6999
Manonthemon <klimowicz.maciej@gmail.com>
2023-04-28 22:43:36 +0100

    Added translations

--- pdftextextractor.py
+++ pdftextextractor.py
@@ -1,4 +1,6 @@
 import os, pyperclip, fitz
+from googletrans import Translator
+translator = Translator()
 
 # Ask the user for the PDF file name or path
 pdf_file = input("Enter the name or path of the PDF file: ")
@@ -20,4 +22,26 @@ for page in pdfFile:
 pyperclip.copy(text)
 
 # Print the text to verify that it was copied correctly
-print(text)
+print("This text has been copied to clipboard" + "\n" + text)
+
+# Ask the user if they want to translate the text
+response = input("Would you like to translate it? (Y/N)")
+
+# Check if the user wants to translate the text
+if response.lower() == "y":
+    target_lang = input("Enter the target language code (e.g., 'es' for Spanish): ")
+
+    # Translate the text to the target language
+    translated = translator.translate(text, dest=target_lang)
+
+    # Access the translated text
+    translated_text = translated.text
+
+    # Copy the translated text to the clipboard
+    pyperclip.copy(translated_text)
+
+    # Print the translated text to verify that it was copied correctly
+    print(translated_text)
+    print("This translation has been copied to the clipboard")
+else:
+    print("Translation skipped.") # Notify user that the translation was skipped
